7e5d801522222ae1c70425ab8df3d083f8045a34
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-20 20:07:22 -0600

    Update mergeIntervals.py

--- mergeIntervals.py
+++ mergeIntervals.py
@@ -8,3 +8,8 @@ class Solution:
         #O(nlogn)
         intervals.sort(key = lambda i : i[0])
         output = [intervals[0]]
+        for start, end in intervals[1:]:
+            lastEnd = output[-1][1]
+
+            if start <= lastEnd:
+                output[-1][1] = max(lastEnd, end)
