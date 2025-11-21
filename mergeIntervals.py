b00fcbe50577b00ed2750d7411feadd9cac62c5a
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-20 20:08:08 -0600

    Update mergeIntervals.py

--- mergeIntervals.py
+++ mergeIntervals.py
@@ -16,3 +16,8 @@ class Solution:
             else:
                 output.append([start,end])
         return output
+
+if __name__ == "__main__":
+    sol = Solution()
+    intervals = [[1,3],[2,6],[8,10],[15,18]]
+    print("Output is : ", sol.mergeIntervals(intervals))
