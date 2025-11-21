028ec2c7274f8f3ca8c7f2aa72cd085f15412666
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-20 20:06:32 -0600

    Update mergeIntervals.py

--- mergeIntervals.py
+++ mergeIntervals.py
@@ -6,3 +6,5 @@ from typing import List
 class Solution:
     def mergeIntervals(self, intervals: List[List[int]]) -> List[List[int]]:
         #O(nlogn)
+        intervals.sort(key = lambda i : i[0])
+        output = [intervals[0]]
